ab4e184958fd01e1e31a4fa6157c13d08acd9dc6
welter <welter@163.com>
2025-11-25 22:30:54 +0800

    阶段性提交，TransController的onInit报错.

--- lib/utils/transController.dart
+++ lib/utils/transController.dart
@@ -3,37 +3,45 @@ import 'package:brainbox/data/trading_database.dart';
 
 class TransController extends GetxController {
   var transactions = <Transaction>[].obs;  // 使用 Rx 类型存储 Todo 列表
-  final TradingDatabase _db = TradingDatabase();
-  late final TransactionDao transactionDao;
+  late final TradingDatabase _db;
+  late final TransactionDao transactionDao ;
   var curIndex=0.obs;
   @override
-  void onInit() {
+  void onInit() async {
+    await _initializeDatabase;
+    await loadTransactions();
     super.onInit();
-    transactionDao=TransactionDao(_db);
-    loadTransactions();
+
   }
 
+  Future<void> _initializeDatabase() async {
+    // 假设 TradingDatabase 的初始化是异步的
+    _db = await TradingDatabase(); // 或者根据实际的异步方法初始化
+    transactionDao=TransactionDao(_db);
+  }
+
+
   // 加载所有交易
-  void loadTransactions() async {
+  Future<void> loadTransactions() async {
 
     List<Transaction> todoMaps = await transactionDao.getTransactions();
     transactions.value = todoMaps;
   }
 
   // 添加交易
-  void addTransaction(Transaction transaction) async {
+  Future<void> addTransaction(Transaction transaction) async {
     await transactionDao.insertTransactions(transactions.value.first);
     loadTransactions();  // 插入后重新加载列表
   }
 
   // 更新交易
-  void updateTransaction(Transaction transaction) async {
+  Future<void> updateTransaction(Transaction transaction) async {
     await transactionDao.updateTransactions(transactions.value.first);
     loadTransactions();  // 更新后重新加载列表
   }
 
   // 删除交易
-  void deleteTodo(int id) async {
+  Future<void> deleteTodo(int id) async {
     await transactionDao.deleteTransactions(id);
     loadTransactions();  // 删除后重新加载列表
   }
